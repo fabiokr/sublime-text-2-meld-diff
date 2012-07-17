bb0fc9c8616782455c8215721d0a108b7fd2a170
Michał Prochowski <mablo@mablo.info>
2012-07-17 14:27:12 +0200

    Initial commit

--- meld-diff.py
+++ meld-diff.py
@@ -5,6 +5,13 @@ MELD_BIN_PATH = '/usr/bin/meld'
 
 class MeldDiffCommand(sublime_plugin.WindowCommand):
     def run(self, files):
-        if len(files) != 2:
-            return
-        os.system('%s "%s" "%s" &' %(MELD_BIN_PATH, files[0], files[1]))
+        lenFiles = len(files)
+        if (lenFiles == 2):
+            os.system('%s "%s" "%s" &' %(MELD_BIN_PATH, files[0], files[1]))
+        if (lenFiles == 3):
+            os.system('%s "%s" "%s" "%s" &' %(MELD_BIN_PATH, files[0], files[1], files[2]))
+        return
+
+    def is_visible(self, files):
+        lenFiles = len(files)
+        return (lenFiles >= 2 and lenFiles <= 3)
